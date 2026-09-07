3a1562f9730ebaf1868b27eb297a0fe3095f549d
TCOUS1 <compotee.affluences_39@icloud.com>
2026-09-07 18:51:56 +0200

    msdgn2.1

--- terminale-stmg/msdgn/2.1/skin/js/skin.js
+++
@@ -1,7 +0,0 @@
-// Skin specific Javascript
-sc$("content").addEventListener("scroll", function (pEvt) {
-	const vCurrentScrollPos = this.scrollTop;
-	if (sc$("content").fPrevScrollpos < vCurrentScrollPos) document.body.classList.add("scrollingDown");
-	else document.body.classList.remove("scrollingDown");
-	sc$("content").fPrevScrollpos = vCurrentScrollPos;
-});
